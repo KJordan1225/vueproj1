c38c3568aad4d7620ebba6db92ca648ec54badc6
KJordan1225 <shadow902@gmail.com>
2023-07-06 12:02:51 -0400

    MAIN-upto EMITS

---
+++ src/components/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+    <teleport to=".modals-container">
+        <div class="modal">
+            <h1>{{ title }}</h1>
+            <slot />
+            <button @click="$emit('hideModal')">Hide Modal</button>
+        </div>
+    </teleport>
+</template>
+
+<script setup>
+
+/*
+    Props
+*/
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: 'No Title Specified'}
+})  
+
+/*
+    emits
+*/
+
+const emit = defineEmits(['hideModal'])
+
+
+</script>
+
+<style>
+.modal {
+    background: beige;
+    padding: 10px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100;
+    height:100%;
+}
+</style>
+
---
+++ src/views/ModalsView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="modals">
+        <h1>Modals</h1>
+        <button @click="showModal = true">Show Modal</button>   
+        <Modal
+          v-if="showModal" 
+          @hideModal = "showModal = false"         
+          >            
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque temporibus consequuntur, 
+                illum iste molestias cupiditate perspiciatis! Pariatur tempore, beatae, tenetur ipsam 
+                cupiditate dolor laboriosam autem ullam minus ad obcaecati provident!</p>
+        </Modal>  
+    </div>
+</template>
+
+<script setup>
+    /*
+        Imports
+    */
+    
+    import { ref } from 'vue'   
+    import Modal from '@/components/Modal.vue'     
+
+
+    /*
+        Modals
+    */
+    
+    const showModal = ref(false)
+
+</script>
+
